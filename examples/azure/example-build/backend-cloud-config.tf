9a1fc79fcced95d5767be015610db479390f465b
Roman Sokolkov <roman@giantswarm.io>
2017-12-29 12:03:30 +0100

    Use local state for cloud configs

--- examples/azure/example-build/backend-cloud-config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "<cluster_name>terraform"
-    container_name       = "<cluster_name>-state"
-    key                  = "terraform-cloud-config"
-  }
-}
--- platforms/azure/giantnetes-cloud-config/backend.tf
+++
@@ -1,1 +0,0 @@
-../../../build/backend-cloud-config.tf
