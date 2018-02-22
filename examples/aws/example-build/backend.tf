171571754ecef036e4d06a5f4fb8f943070bc242
Roman Sokolkov <roman@giantswarm.io>
2018-02-22 14:37:11 +0100

    Add support for aws profiles

--- examples/aws/example-build/backend.tf
+++ examples/aws/example-build/backend.tf
@@ -2,8 +2,11 @@
 terraform {
   backend "s3" {
     bucket         = "<cluster name>-state"
-    key            = "terraform.tfstate"
-    region         = "<aws region>"
     dynamodb_table = "<cluster name>-lock"
+    key            = "terraform.tfstate"
+
+    # Make sure to define profile in ~/.aws/config
+    profile = "<cluster name>"
+    region  = "<aws region>"
   }
 }
---
+++ examples/aws/example-build/profile.tf
@@ -0,0 +1,5 @@
+# Replace <cluster name> with proper values.
+provider "aws" {
+  # Make sure to define profile in ~/.aws/config
+  profile = "<cluster name>"
+}
