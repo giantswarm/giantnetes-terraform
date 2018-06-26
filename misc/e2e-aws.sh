0466a3d53c79b917c7adc61da1be32c4bb7b31e3
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-06-26 15:35:06 +0300

    Remove testing bramch

--- misc/e2e-aws.sh
+++ misc/e2e-aws.sh
@@ -167,7 +167,7 @@ stage-vault() {
     # Download Ansible playbooks for Vault bootstrap.
     local tmp=$(mktemp -d)
     cd $tmp
-    git clone --depth 1 --quiet https://taylorbot:${E2E_GITHUB_TOKEN}@github.com/giantswarm/hive.git -b vault_0.10.3
+    git clone --depth 1 --quiet https://taylorbot:${E2E_GITHUB_TOKEN}@github.com/giantswarm/hive.git
     cd hive
 
     # Prepare configuration for Ansible.
