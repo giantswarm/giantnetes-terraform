f5bd24ff6fa315f7ccd44ea49cf5c9797113409f
Vaclav Rozsypalek <rozsypalek.vaclav@gmail.com>
2018-07-26 11:05:31 +0200

    vault

--- modules/aws/vault/vault-elb.tf
+++ modules/aws/vault/vault-elb.tf
@@ -14,9 +14,9 @@ resource "aws_elb" "vault" {
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout             = 5
+    timeout             = 4
     target              = "TCP:${var.vault_port}"
-    interval            = 15
+    interval            = 5
   }
 
   tags {
