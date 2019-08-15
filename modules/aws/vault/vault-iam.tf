e68b09f64cb04ccf650f64d874a20cd83e2b5fb8
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-08-16 02:21:28 +0300

    Fix broken vault iam

--- modules/aws/vault/vault-iam.tf
+++ modules/aws/vault/vault-iam.tf
@@ -23,11 +23,33 @@ resource "aws_iam_role" "vault" {
 EOF
 }
 
+resource "aws_iam_role_policy" "vault-s3-ignition" {
+  name = "${var.cluster_name}-vault-s3-ignition"
+  role = "${aws_iam_role.vault.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "arn:${var.arn_region}:s3:::${var.aws_account}-${var.cluster_name}-ignition/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_instance_profile" "vault" {
   name = "${var.cluster_name}-vault"
   role = "${aws_iam_role.vault.name}"
 
-   lifecycle {
+  lifecycle {
     create_before_destroy = true
   }
 }
