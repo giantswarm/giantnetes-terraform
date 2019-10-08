5a19fb413ec904f1a6643be7006857e6041f39f3
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-10-08 18:11:45 +0300

    Remove legacy

--- modules/ignition/render-files.tf
+++
@@ -1,6 +0,0 @@
-resource "template_dir" "config" {
-  source_dir      = "../../../templates/files"
-  destination_dir = "../../../files"
-
-  vars = "${var.ignition_data}"
-}
--- modules/ignition/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "ignition_data" {
-    type = map
-    description = "Data for rendering files, which will be injected as bas64 strings into main template"
-}
