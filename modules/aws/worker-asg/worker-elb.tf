e784f72f0e8ba9da1e5015aeace28401cc179fae
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-02-11 17:36:23 +0200

    Bring back single worker elb

---
+++ modules/aws/worker-asg/worker-elb.tf
@@ -0,0 +1,100 @@
+resource "aws_elb" "worker" {
+  name                      = "${var.cluster_name}-worker"
+  cross_zone_load_balancing = true
+  internal                  = false
+  subnets                   = var.elb_subnet_ids
+  security_groups           = ["${aws_security_group.worker_elb.id}"]
+
+  listener {
+    instance_port     = 30010
+    instance_protocol = "tcp"
+    lb_port           = 80
+    lb_protocol       = "tcp"
+  }
+
+  listener {
+    instance_port     = 30011
+    instance_protocol = "tcp"
+    lb_port           = 443
+    lb_protocol       = "tcp"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 4
+    target              = "TCP:30011"
+    interval            = 5
+  }
+
+  tags = merge(
+    local.common_tags,
+    map(
+      "Name", "${var.cluster_name}-worker"
+    )
+  )
+}
+
+resource "aws_proxy_protocol_policy" "worker" {
+  load_balancer  = aws_elb.worker.name
+  instance_ports = ["30010", "30011"]
+}
+
+resource "aws_security_group" "worker_elb" {
+  name   = "${var.cluster_name}-worker-elb"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    map(
+      "Name", "${var.cluster_name}-worker-elb"
+    )
+  )
+}
+
+resource "aws_route53_record" "worker-wildcard" {
+  count   = var.route53_enabled ? 1 : 0
+  zone_id = var.public_dns_zone_id
+  name    = "*"
+  type    = "A"
+
+  alias {
+    name                   = aws_elb.worker.dns_name
+    zone_id                = aws_elb.worker.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "worker-ingress" {
+  count   = var.route53_enabled ? 1 : 0
+  zone_id = var.public_dns_zone_id
+  name    = var.ingress_dns
+  type    = "A"
+
+  alias {
+    name                   = aws_elb.worker.dns_name
+    zone_id                = aws_elb.worker.zone_id
+    evaluate_target_health = false
+  }
+}
