f8fe9a93c0e4a1c72f3d290ece925b3c845cfa55
Christian Bianchi <christian@giantswarm.io>
2020-03-31 11:00:47 +0200

    Improve installation docs (#309)
    A few improvements in the deployment process

--- examples/azure/bootstrap.sh
+++ examples/azure/bootstrap.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Remove synced states
+rm .terraform -Rf
+
+# Unset all the previously exported vars
+exported_vars=$(env | grep TF_VAR | cut -d "=" -f1)
+if [ "${exported_vars}" = $'\n' ]; then
+  while read -r exported_var; do
+    unset ${exported_var}
+  done <<< ${exported_vars}
+fi
+
+# opsctl path
+OPSCTL_PATH=${OPSCTL_PATH:-opsctl}
+
 # This file overrides terraform.tfvars values.
 
 # azure location without spaces (e.g. westeurope)
@@ -6,7 +22,7 @@ export TF_VAR_azure_location=<value>
 export TF_VAR_cluster_name=<value>
 
 # Leave empty at the beginning. After you'll have vault deployed, you will need to copy the "VAULT TOKEN for the g8s host cluster" in here.
-export TF_VAR_nodes_vault_token=
+export TF_VAR_nodes_vault_token="$(${OPSCTL_PATH} show secret -i ./terraform-secrets.yaml -k Terraform.VaultToken)"
 
 # service principal credentials
 export TF_VAR_azure_sp_tenantid=<value>
@@ -15,25 +31,10 @@ export TF_VAR_azure_sp_subscriptionid=<value>
 # equals to azure appId
 export TF_VAR_azure_sp_aadclientid=<value>
 
-# interactively ask for secret keys
-if [ -z ${TF_VAR_azure_sp_aadclientsecret} ]; then
-  echo "Please enter the password of the service principal:"
-  read TF_VAR_azure_sp_aadclientsecret
-  export TF_VAR_azure_sp_aadclientsecret
-fi
 
-if [ -z ${ARM_ACCESS_KEY} ]; then
-  echo "Please enter secret key for storage account:"
-  read ARM_ACCESS_KEY
-  export ARM_ACCESS_KEY
-fi
+export TF_VAR_azure_sp_aadclientsecret="$(${OPSCTL_PATH} show secret -i ./terraform-secrets.yaml -k Terraform.AzureSPAadClientSecret)"
+export ARM_ACCESS_KEY="$(${OPSCTL_PATH} show secret -i ./terraform-secrets.yaml -k Terraform.ArmAccessKey)"
 
-# example is a standard gigantic.io domain structure.
-# pls don't change if this cluster is installed with gigantic.io
-#
-# if you'd like to configure a custom domain please only change
-# TF_VAR_base_domain (replace "azure.gigantic.io" with the custom
-# domain)
 export TF_VAR_base_domain=${TF_VAR_cluster_name}.${TF_VAR_azure_location}.azure.gigantic.io
 # hosted zone name. This is the zone where the delegated NS records are set.
 # If the installation is in the same subscription, populate the following variable.
@@ -42,9 +43,9 @@ export TF_VAR_root_dns_zone_name="azure.gigantic.io"
 
 # To enable Site-To-Site IPSec uncomment following options. Make sure that bastion subnet is unique across installations.
 # export TF_VAR_vpn_enabled=1
-# export TF_VAR_vpn_right_gateway_address_0=<ip address of first IPSec server>
-# export TF_VAR_vpn_right_gateway_address_1=<ip address of second IPSec server>
-# export TF_VAR_bastion_cidr=<bastion subnet>
+# export TF_VAR_vpn_right_gateway_address_0=185.102.95.187
+# export TF_VAR_vpn_right_gateway_address_1=95.179.153.65
+# export TF_VAR_bastion_cidr=<bastion subnet i.e. 10.0.4.112/28>
 
 # Override here any option from platforms/azure/variables.tf
 
