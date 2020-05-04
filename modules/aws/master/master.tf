eb6ccd88f13c1bfd96e249fdf41f5ef622fb420e
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-05-04 16:35:48 +0300

    Add internal g8s elb

--- modules/aws/master/master.tf
+++ modules/aws/master/master.tf
@@ -3,9 +3,14 @@ locals {
     "giantswarm.io/installation", "${var.cluster_name}",
     "kubernetes.io/cluster/${var.cluster_name}", "owned"
   )}"
-  customer_vpn_subnets              = var.customer_vpn_subnets != "" ? split(",", var.customer_vpn_subnets) : []
-  k8s_api_internal_access_whitelist = concat(["${var.aws_cni_cidr_block}", "${var.vpc_cidr}"], "${var.nat_gateway_public_ips}")
-  k8s_api_external_access_whitelist = concat(["${var.external_ipsec_public_ip_0}/32", "${var.external_ipsec_public_ip_1}/32"], local.customer_vpn_subnets)
+  customer_vpn_public_subnets = var.customer_vpn_public_subnets != "" ? split(",", var.customer_vpn_public_subnets) : []
+  customer_vpn_private_subnets = var.customer_vpn_private_subnets != "" ? split(",", var.customer_vpn_private_subnets) : []
+  # k8s_api prefixed values represent access to public loadbalancer
+  k8s_api_internal_access_whitelist = concat(["${var.aws_cni_cidr_block}","${var.vpc_cidr}"], "${var.nat_gateway_public_ips}")
+  k8s_api_external_access_whitelist = concat(["${var.external_ipsec_public_ip_0}/32", "${var.external_ipsec_public_ip_1}/32"], local.customer_vpn_public_subnets)
+  # k8s_api_internal prefixed values represent access to private loadbalancer
+  k8s_api_internal_internal_access_whitelist = concat(["${var.aws_cni_cidr_block}","${var.vpc_cidr}"], "${var.nat_gateway_public_ips}")
+  k8s_api_internal_external_access_whitelist = concat(["${var.external_ipsec_public_ip_0}/32", "${var.external_ipsec_public_ip_1}/32"], local.customer_vpn_private_subnets)
 }
 
 data "aws_availability_zones" "available" {}
