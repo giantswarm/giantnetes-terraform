b0a9307b94b62e697112c62cd37295de6103ec84
calvix <vaclav@giantswarm.io>
2020-09-15 15:11:27 +0200

    rework-aws-cni-subnets1 (#383)

--- modules/aws/vpc/vpc-subnet-cni.tf
+++ modules/aws/vpc/vpc-subnet-cni.tf
@@ -19,10 +19,31 @@ resource "aws_subnet" "cni" {
   depends_on = [aws_vpc_ipv4_cidr_block_association.cni]
 }
 
-resource "aws_route_table_association" "cni" {
-  count = length(var.aws_cni_pod_cidrs)
+resource "aws_vpc_ipv4_cidr_block_association" "cni_v2" {
+  vpc_id     = aws_vpc.cluster_vpc.id
+  cidr_block = var.aws_cni_cidr_v2
+}
 
-  subnet_id      = aws_subnet.cni[count.index].id
+resource "aws_subnet" "cni_v2" {
+  count = length(var.aws_cni_subnets_v2)
+
+  vpc_id            = aws_vpc.cluster_vpc.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = var.aws_cni_subnets_v2[count.index]
+
+  tags = merge(
+    local.common_tags,
+    map(
+      "Name", "${var.cluster_name}-cni_v2-${count.index}"
+    )
+  )
+  depends_on = [aws_vpc_ipv4_cidr_block_association.cni_v2]
+}
+
+resource "aws_route_table_association" "cni" {
+  count = length(var.aws_cni_subnets_v2)
+
+  subnet_id      = aws_subnet.cni_v2[count.index].id
   route_table_id = aws_route_table.cluster_vpc_private[count.index].id
 }
 
@@ -36,7 +57,7 @@ resource "aws_security_group" "cni" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["${var.vpc_cidr}", "${var.aws_cni_cidr_block}"]
+    cidr_blocks = ["${var.vpc_cidr}", "${var.aws_cni_cidr_v2}"]
   }
 
   egress {
