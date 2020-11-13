f94df8dff16def66df751aa3d98f66504d223a12
vol <me@volodymyrstoiko.com>
2020-11-13 14:33:21 +0200

    Bring back old flatcar

--- platforms/aws/giantnetes/variables.tf
+++ platforms/aws/giantnetes/variables.tf
@@ -135,7 +135,7 @@ variable "flatcar_linux_channel" {
 variable "flatcar_linux_version" {
   description = "Flatcar linux version."
   type        = string
-  default     = "2605.7.0"
+  default     = "2512.2.1"
 }
 
 variable "flatcar_ami_owner" {
--- platforms/azure/giantnetes/variables.tf
+++ platforms/azure/giantnetes/variables.tf
@@ -121,7 +121,7 @@ variable "flatcar_linux_channel" {
 variable "flatcar_linux_version" {
   description = "Flatcar linux version."
   type        = string
-  default     = "2605.7.0"
+  default     = "2512.2.1"
 }
 
 variable "vault_image_publisher" {
