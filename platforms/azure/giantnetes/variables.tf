828d28825fcd4c53bfa286fc6138d94f3cc78f38
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-12-10 00:26:31 +0200

    Fix azurerm provider environment (#425)
    * Fix azure public cloud env
    
    * Revert "Fix azure public cloud env"
    
    This reverts commit 9ddfdbfaeb99d44dd75553df07eb39013e5f43c5.
    
    * Add environment variable for azurerm configuration

--- platforms/azure/giantnetes/variables.tf
+++ platforms/azure/giantnetes/variables.tf
@@ -21,6 +21,11 @@ variable "azure_location" {
 
 variable "azure_cloud" {
   description = "Azure cloud to use. Possible values can be found here: https://github.com/Azure/go-autorest/blob/ec5f4903f77ed9927ac95b19ab8e44ada64c1356/autorest/azure/environments.go#L13"
+  default     = "AzurePublicCloud"
+}
+
+variable "environment" {
+  description = "Azure environment to use. Possible values can be found here: https://www.terraform.io/docs/backends/types/azurerm.html#environment"
   default     = "public"
 }
 
