69e9d162a97e88da129c86bd6223dced86b6fba9
calvix <vaclav@giantswarm.io>
2021-11-25 15:04:46 +0100

    fixs3-object-tags

--- modules/aws/bastion/bastion.tf
+++ modules/aws/bastion/bastion.tf
@@ -122,7 +122,7 @@ resource "aws_s3_bucket_object" "ignition_bastion_with_tags" {
   server_side_encryption = "AES256"
 
   tags = merge(
-    local.common_tags,
+    var.additional_tags,
     map(
       "Name", "${var.cluster_name}-ignition-bastion"
     )
