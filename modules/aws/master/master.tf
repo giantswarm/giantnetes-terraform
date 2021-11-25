c9eba572485db503d4b5094d0e834d5dd248c760
calvix <vaclav@giantswarm.io>
2021-11-25 14:16:39 +0100

    f

--- modules/aws/master/master.tf
+++ modules/aws/master/master.tf
@@ -4,6 +4,7 @@ locals {
     map(
       "giantswarm.io/cluster", var.cluster_name,
       "giantswarm.io/installation", var.cluster_name,
+      "giantswarm.io/cluster-type", "control-plane",
       "kubernetes.io/cluster/${var.cluster_name}", "owned"
     )
   )
