e61a72e2b4cd7d36490c0bbd04aa34aba2b2388d
Christian Bianchi <whites11@gmail.com>
2021-12-15 07:45:50 +0100

    bump flatcar and kubernetes

--- platforms/azure/giantnetes/variables.tf
+++ platforms/azure/giantnetes/variables.tf
@@ -128,7 +128,7 @@ variable "flatcar_linux_channel" {
 variable "flatcar_linux_version" {
   description = "Flatcar linux version."
   type        = string
-  default     = "2765.2.2"
+  default     = "2983.2.0"
 }
 
 variable "vault_image_publisher" {
@@ -152,7 +152,7 @@ variable "docker_registry" {
 }
 variable "hyperkube_version" {
   type    = string
-  default = "1.20.5"
+  default = "1.20.13"
 }
 
 ### DNS ###
