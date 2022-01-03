056f9ada208fd2c01363e8beb5615bef78e428d7
calvix <vaclav@giantswarm.io>
2022-01-03 10:07:24 +0100

    bump versions

--- platforms/aws/giantnetes/variables.tf
+++ platforms/aws/giantnetes/variables.tf
@@ -130,7 +130,7 @@ variable "flatcar_linux_channel" {
 variable "flatcar_linux_version" {
   description = "Flatcar linux version."
   type        = string
-  default     = "3033.2.0"
+  default     = "2905.2.6"
 }
 
 variable "flatcar_ami_owner" {
