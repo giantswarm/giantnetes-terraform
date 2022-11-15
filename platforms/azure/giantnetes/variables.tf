073fbaf328baad395af5d4fef006b419d99a0280
Christian Bianchi <whites11@gmail.com>
2022-11-15 09:47:24 +0100

    - Revert flatcar to 3227.2.1 to prevent filesystem hang bug (see https://github.com/giantswarm/giantswarm/issues/23831). (#676)
    - Bump kubernetes to 1.24.8.

--- platforms/azure/giantnetes/variables.tf
+++ platforms/azure/giantnetes/variables.tf
@@ -138,7 +138,7 @@ variable "flatcar_linux_channel" {
 variable "flatcar_linux_version" {
   description = "Flatcar linux version."
   type        = string
-  default     = "3227.2.3"
+  default     = "3227.2.1"
 }
 
 variable "vault_image_publisher" {
@@ -168,7 +168,7 @@ variable "docker_registry_mirror" {
 
 variable "hyperkube_version" {
   type    = string
-  default = "1.24.7"
+  default = "1.24.8"
 }
 
 variable "pod_infra_image" {
