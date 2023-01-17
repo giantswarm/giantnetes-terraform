549bb1dff6225a7637b30545813726ee84c7c253
Christian Bianchi <whites11@gmail.com>
2023-01-17 15:34:09 +0100

    Bump + registry mirror (#683)
    * Bump components
    
    * add registry mirror setting to docker daemon
    
    * Change api server resources calculation to leave more room for pods on master nodes.

--- platforms/aws/giantnetes/variables.tf
+++ platforms/aws/giantnetes/variables.tf
@@ -130,7 +130,7 @@ variable "flatcar_linux_channel" {
 variable "flatcar_linux_version" {
   description = "Flatcar linux version."
   type        = string
-  default     = "3227.2.1"
+  default     = "3374.2.2"
 }
 
 variable "flatcar_ami_owner" {
@@ -150,7 +150,7 @@ variable "docker_registry_mirror" {
 
 variable "hyperkube_version" {
   type    = string
-  default = "1.24.8"
+  default = "1.24.9"
 }
 
 ### DNS ###
